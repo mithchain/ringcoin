c70ebf547adae53b0f528c86094606cfac354ac5
mithchain <mithchain@163.com>
2025-05-02 15:24:25 +0800

    Update params.h

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -16,42 +16,6 @@
 
 namespace Consensus {
 
-enum DeploymentPos
-{
-    DEPLOYMENT_TESTDUMMY,
-    DEPLOYMENT_CSV, // Deployment of BIP68, BIP112, and BIP113.
-    DEPLOYMENT_SEGWIT, // Deployment of BIP141, BIP143, and BIP147.
-    // NOTE: Also add new deployments to VersionBitsDeploymentInfo in versionbits.cpp
-    MAX_VERSION_BITS_DEPLOYMENTS
-};
-
-/**
- * Struct for each individual consensus rule change using BIP9.
- */
-struct BIP9Deployment {
-    /** Bit position to select the particular bit in nVersion. */
-    int bit;
-    /** Start MedianTime for version bits miner confirmation. Can be a date in the past */
-    int64_t nStartTime;
-    /** Timeout/expiry MedianTime for the deployment attempt. */
-    int64_t nTimeout;
-
-    /** Constant for nTimeout very far in the future. */
-    static constexpr int64_t NO_TIMEOUT = std::numeric_limits<int64_t>::max();
-
-    /** Special value for nStartTime indicating that the deployment is always active.
-     *  This is useful for testing, as it means tests don't need to deal with the activation
-     *  process (which takes at least 3 BIP9 intervals). Only tests that specifically test the
-     *  behaviour during activation cannot use this. */
-    static constexpr int64_t ALWAYS_ACTIVE = -1;
-};
-
-/**
- * Parameters that influence chain consensus.
- */
-
-const int FOREIGN_COIN_COUNT = 4;   // Ring-fork: Foreign key count
-
 struct Params {
     uint256 hashGenesisBlock;
     //int nSubsidyHalvingInterval;  // Ring-fork: No halving on chain
